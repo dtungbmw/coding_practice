8802a8953401ec19ad93c2a8d6a3a0a668eb012e
david <dtung@yahoo.com>
2020-06-06 00:37:53 -0700

    update

---
+++ string_perm.py
@@ -0,0 +1,15 @@
+
+
+def perm(lst):
+    if len(lst) ==0:
+        return []
+    elif len(lst) == 1:
+        return [lst]
+    else:
+        l = []
+        for i in range( len(lst)):
+            x = lst[i]
+            xs = lst [:i] + lst [i+1:]
+            for p in perm(xs):
+                l.append([x]+p)
+        return l
