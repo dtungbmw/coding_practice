d694cfa443fdc42d93d2f04c813e96ff7a3d30b0
davtung <davtung@cisco.com>
2021-02-23 13:34:48 -0800

    update

--- python/P10-letter_phone.py
+++ python/P10-letter_phone.py
@@ -4,5 +4,28 @@
 # 地界：你的熱情與世界的需要相遇的地方
 
 
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        output = []
+        L = len(digits)
+        mp = {
+            '2': ['a', 'b', 'c'],
+            '3': ['d', 'e', 'f'],
+            '4': ['g', 'h', 'i'],
+            '5': ['j', 'k', 'l'],
+            '6': ['m', 'n', 'o'],
+            '7': ['p', 'q', 'r', 's'],
+            '8': ['t', 'u', 'v'],
+            '9': ['w', 'x', 'y', 'z']
+        }
+        def dfs(word, idx):
+            if idx == L:
+                output.append(word)
+            else:
+                for c in mp[digits[idx]]:
+                    dfs(word + c, idx + 1)
+        if L != 0:
+            dfs("", 0)
+        return output
 
 
--- python/P6-word_break.py
+++ python/P6-word_break.py
@@ -5,4 +5,22 @@
 #if s can be segmented into a space-separated sequence of one or more dictionary words.
 
 def isBreak():
+    "here mem[idx] refer to if s[:idx+1] can be segmented into words"
 
+    def recurse(idx, mem):
+        if (idx) == len(s):
+            return True
+        if mem[idx] != 0:
+            return mem[idx]
+        for word in wordDict:
+            "apple.. in [apple, pen]"
+            if s[idx:idx + len(word)] == word:
+                mem[idx] = True
+                if recurse(idx + len(word), mem):
+                    mem[idx + len(word)] = True
+        return mem[len(s)]
+
+    if not s:
+        return False
+    mem = [0] * (len(s) + 1)
+    return recurse(0, mem)
