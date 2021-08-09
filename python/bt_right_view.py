94227646a3ea791a5eccd2bc9d739d8e4257b71e
David Tung (davtung) <davtung@cisco.com>
2021-08-08 19:51:07 -0700

    update

--- python/bt_right_view.py
+++ python/bt_right_view.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -5,26 +7,46 @@ class TreeNode:
         self.right = right
 
 class Solution:
-    def rightSideView(self, root: TreeNode) -> List[int]:
+
+    def initTree(self):
+        r = TreeNode()
+        r.val = 1
+        r.left = TreeNode()
+        r.left.val = 2
+        r.right = TreeNode()
+        r.right.val = 3
+
+        return r
+
+    def calLevel(self, queue, curr_size):
+        level = []
+        for _ in range(curr_size):
+            node = queue.popleft()
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+            level.append(node.val)
+
+        return level
+
+    def rightSideView(self, root: TreeNode) :
         if not root:
             return None
         queue = deque()
         queue.append(root)
         res = []
+
         while queue:
-            level = []
-            for _ in range(len(queue)):
-                node = queue.popleft()
-
-                if node.left:
-                    queue.append(node.left)
-
-                if node.right:
-                    queue.append(node.right)
-
-                level.append(node.val)
-
+            level = self.calLevel(queue, len(queue))
             res.append(level[-1])
 
         return res
 
+
+
+s = Solution()
+root = s.initTree()
+res = s.rightSideView(root)
+
+print(res)
