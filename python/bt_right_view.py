8d487ce7c08aecea71935cb25a0b546a3ba693f2
David Tung (davtung) <davtung@cisco.com>
2021-08-18 09:00:43 -0700

    update

--- python/bt_right_view.py
+++ python/bt_right_view.py
@@ -1,12 +1,43 @@
 from collections import deque
+from typing import *
 
+# Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 
+
 class Solution:
+
+    def computeLevel(self, queue):
+        size = len(queue)
+        level = []
+        for _ in range(size):
+            node = queue.popleft()
+            level.append(node.val)
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+
+        return level
+
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+
+        if not root:
+            return None
+
+        queue = deque()
+        queue.append(root)
+        res = []
+
+        while queue:
+            level = self.computeLevel(queue)
+            res.append(level[-1])
+
+        return res
 
     def initTree(self):
         r = TreeNode()
@@ -18,35 +49,9 @@ class Solution:
 
         return r
 
-    def calLevel(self, queue, curr_size):
-        level = []
-        for _ in range(curr_size):
-            node = queue.popleft()
-            if node.left:
-                queue.append(node.left)
-            if node.right:
-                queue.append(node.right)
-            level.append(node.val)
-
-        return level
-
-    def rightSideView(self, root: TreeNode) :
-        if not root:
-            return None
-        queue = deque()
-        queue.append(root)
-        res = []
-
-        while queue:
-            level = self.calLevel(queue, len(queue))
-            res.append(level[-1])
-
-        return res
-
-
-
 s = Solution()
 root = s.initTree()
 res = s.rightSideView(root)
 
 print(res)
+
